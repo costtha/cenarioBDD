018b3541248186e402b8b5b332968b0a6640fbbd
Diego <diegocostaij@gmail.com>
2024-04-05 19:52:00 -0300

    adição de novos arquivos

--- cypress/e2e/inserir-todo.cy.js
+++ cypress/e2e/inserir-todo.cy.js
@@ -2,14 +2,14 @@ describe("Criar uma nova tarefa", () => {
     it("Deve ser possível adicionar uma nova tarefa", () => {
         const tituloDaTarefa = "Compra pão";
 
-        cy.visit("http://127.0.0.1:5500/index.html");
+        cy.visit("http://127.0.0.1:5500/todo-list-alpine-js/index.html");
         cy.get('#digite_sua_tarefa').click().type(tituloDaTarefa);
         cy.get('#criar_tarefa').click();
         cy.contains('.tarefas-cadastradas', tituloDaTarefa);
     });
 
     it("Deve exibir mensagem de erro ao tentar criar uma tarefa sem digitar nada", () => {
-        cy.visit("http://127.0.0.1:5500/index.html");
+        cy.visit("http://127.0.0.1:5500/todo-list-alpine-js/index.html");
         cy.get('#criar_tarefa').click();
         cy.contains('.erro', 'Campo de tarefa é obrigatório');
         cy.get('.tarefas-cadastradas').should('not.exist');
@@ -18,7 +18,7 @@ describe("Criar uma nova tarefa", () => {
     it("Deve exibir mensagem de erro ao tentar criar uma tarefa com caracteres inválidos", () => {
         const tarefaComCaracteresInvalidos = "Tarefa com caracteres inválidos @!#";
 
-        cy.visit("http://127.0.0.1:5500/index.html");
+        cy.visit("http://127.0.0.1:5500/todo-list-alpine-js/index.html");
         cy.get('#digite_sua_tarefa').click().type(tarefaComCaracteresInvalidos);
         cy.get('#criar_tarefa').click();
         cy.contains('.erro', 'Tarefa contém caracteres inválidos');
@@ -28,7 +28,7 @@ describe("Criar uma nova tarefa", () => {
     it("Deve cancelar a operação de criação de tarefa ao clicar em 'Cancelar'", () => {
         const tituloDaTarefa = "Compra pão";
 
-        cy.visit("http://127.0.0.1:5500/index.html");
+        cy.visit("http://127.0.0.1:5500/todo-list-alpine-js/index.html");
         cy.get('#digite_sua_tarefa').click().type(tituloDaTarefa);
         cy.get('#cancelar').click();
         cy.get('.tarefas-cadastradas').should('not.exist');
@@ -37,7 +37,7 @@ describe("Criar uma nova tarefa", () => {
     it("Deve redirecionar para a página de tarefas cadastradas após criar uma nova tarefa", () => {
         const tituloDaTarefa = "Compra pão";
 
-        cy.visit("http://127.0.0.1:5500/index.html");
+        cy.visit("http://127.0.0.1:5500/todo-list-alpine-js/index.html");
         cy.get('#digite_sua_tarefa').click().type(tituloDaTarefa);
         cy.get('#criar_tarefa').click();
         cy.url().should('include', '/tarefas-cadastradas');
@@ -47,7 +47,7 @@ describe("Criar uma nova tarefa", () => {
     it("Deve exibir mensagem de erro ao tentar criar uma tarefa com uma quantidade excessiva de caracteres", () => {
         const tarefaComExcessoDeCaracteres = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
-        cy.visit("http://127.0.0.1:5500/index.html");
+        cy.visit("http://127.0.0.1:5500/todo-list-alpine-js/index.html");
         cy.get('#digite_sua_tarefa').click().type(tarefaComExcessoDeCaracteres);
         cy.get('#criar_tarefa').click();
         cy.contains('.erro', 'Tarefa excede o limite de caracteres permitido');
@@ -57,7 +57,7 @@ describe("Criar uma nova tarefa", () => {
     it("Deve verificar se a nova tarefa está presente na lista de tarefas cadastradas após a criação", () => {
         const tituloDaTarefa = "Compra pão";
 
-        cy.visit("http://127.0.0.1:5500/index.html");
+        cy.visit("http://127.0.0.1:5500/todo-list-alpine-js/index.html");
         cy.get('#digite_sua_tarefa').click().type(tituloDaTarefa);
         cy.get('#criar_tarefa').click();
         cy.contains('.tarefas-cadastradas', tituloDaTarefa);
--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,8 +1,8 @@
 describe("Carregamento da aplicação", () => {
 
     it("Deve ser possivel carregar a aplicação", () => {
-        cy.visit("http://127.0.0.1:5500/index.html")
-
+        cy.visit("http://127.0.0.1:5500/todo-list-alpine-js/index.html")
+        cy.get('h1').should("have.text", "O que fazer hoje?")
     })
 
 } )
